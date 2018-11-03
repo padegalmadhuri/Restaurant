a7ba066aa606aa16f99732f5b805a996f4ca6d65
padegalmadhuri <35970571+padegalmadhuri@users.noreply.github.com>
2018-11-03 16:43:50 +0530

    Add files via upload

---
+++ js/sw_registration.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./sw.js')
+	.then(function() {
+		console.log('Registration worked!');
+	})
+	.catch(function() {
+		console.log('Registration failed!');
+	});
